31002a58b0e4fd42169847e7921191afc3542ecc
Pedro <bellottipcpedro@gmail.com>
2019-08-20 11:47:42 -0300

    Alterando codigo e adicionando script de grafico

--- cliente.py
+++ cliente.py
@@ -6,11 +6,17 @@ from threading import Timer
 np.random.seed (5)
 
 #Cabecalho
-print ("Timestamp, IpOrigem, PortaOrigem, IpDestino, PortaDestino, ?, Tempo, BitsEnviados, Banda, Jitter, PctPerdido, PctEnviado, %Perda, ForaDeOrdem")
+print ("Timestamp,IpOri,PortaOri,IpDest,PortaDest,?,Tempo,BitsEnv,Banda,Jitter,PctPerdido,PctEnv,%Perda,ForaOrdem")
+
+def ping(id):
+    #os.system("ping 10.1.0.1 -c 5 >> ping_%d.txt" % (id))
+    print("ping 10.1.0.1 -c 5 >> ping_%d.txt" % (id))
+    t = Timer(10, ping, [id+1])
+    t.start()
 
 def executa(comando):
-    os.system(comando)
-    #print (comando)
+    #os.system(comando)
+    print (comando)
 
 def valorExponencial (media):
     return int(np.random.exponential(media))
@@ -18,21 +24,25 @@ def valorExponencial (media):
 def valorNormal (media, desvio):
     return int(np.random.normal(media, desvio))
 
-porta = 5001
-ipserver = '10.1.0.1'
-ipcliente = '10.1.0.2'
-for i in range (10):
-    portacliente = str(porta+500)
-    duracao = '10'
-    banda = str(valorNormal(700,150))
-    u = np.random.randint(1,10000)
-    if (int(portacliente) % 2 == 0):
-        unidade = 'Mbits/sec'
-    else:
-        unidade = 'Kbits/sec'
-    cmd = ('iperf -u -c %s --bind %s:%s -p %s -b %s%s -t %s -y C &' % (ipserver, ipcliente, portacliente, porta, banda, unidade, duracao))
-    tempo = valorExponencial(10)
-    #print ('Executando em %d segundos, banda %s%s' % (tempo, banda, unidade))
-    t = Timer(tempo, executa, [cmd])
-    t.start() # Executa depois do tempo
-    porta += 1
+if __name__ == "__main__":
+    porta = 5001
+    ipserver = '10.1.0.1'
+    ipcliente = '10.1.0.2'
+    for i in range (10):
+        portacliente = str(porta+500)
+        duracao = '10'
+        banda = str(valorNormal(700,150))
+        u = np.random.randint(1,10000)
+        if (int(portacliente) % 2 == 0):
+            unidade = 'Mbits/sec'
+        else:
+            unidade = 'Kbits/sec'
+        cmd = ('iperf -u -c %s --bind %s:%s -p %s -b %s%s -t %s -y C &' % (ipserver, ipcliente, portacliente, porta, banda, unidade, duracao))
+        tempo = valorExponencial(10)
+        print ('Executando em %d segundos, banda %s%s' % (tempo, banda, unidade))
+        t = Timer(tempo, executa, [cmd])
+        t.start() # Executa depois do tempo
+        porta += 1
+    #Inicia o ping apos 5 segundos
+    t = Timer(5, ping, [1])
+    t.start()
