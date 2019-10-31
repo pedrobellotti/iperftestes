544a02d540462c3784e427a9da3957db2246c47f
Pedro <bellottipcpedro@gmail.com>
2019-10-31 11:22:26 -0300

    Adicionando teste com seed diferente

--- cliente.py
+++ cliente.py
@@ -5,7 +5,7 @@ import signal
 import subprocess
 
 #Seed global
-seed = 10
+seed = 20
 np.random.seed (seed)
 
 #Cabecalho
@@ -70,7 +70,7 @@ minDuracao = 5.0
 maxDuracao = 100.0
 quantidade = 2500
 mediaBanda = 1024.0 #1910.0
-mediaTempoInicio = 0.35 #0.15 0.25 0.35
+mediaTempoInicio = 0.25 #0.15 0.25 0.35
 #metodo = "Par/Impar"
 metodo = "SW->HW"
 fInfo = open("info.txt","w+")
